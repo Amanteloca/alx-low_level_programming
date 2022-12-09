67a1abbe287c1713540360c32b429e7e2e12d216
Anthony <tonyanaky51@gmail.com>
2022-12-09 01:14:16 -0800

    3rd commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main ()
+
+{
+
+        char pha;
+
+        for (pha = 'a', pha <= 'z' , pha++)
+        
+	putchar(pha);
+
+	for (pha = 'A', pha <= 'Z' , pha++)
+
+        putchar(pha);
+ 
+
+
+        putchar(\n);
+        return 0;
+}
