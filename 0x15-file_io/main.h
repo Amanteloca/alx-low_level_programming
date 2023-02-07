5e42ca59a072eea4c0543931256263e12ba95ef8
amanteloca <tonyanaky51@gmail.com>
2023-02-06 16:15:07 -0800

    update to code

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,53 +1,14 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include "main.h"
 
-/**
- * read_textfile - reads and prints from a file
- * @filename: path to file
- * @letters: chars to read
- * Return: chars read
- */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	int fd;
-	char *buff;
-	ssize_t bytes, r;
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
-	if (!filename)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		close(fd);
-		return (0);
-	}
-
-	buff = malloc(sizeof(char) * letters);
-	if (!buff)
-	{
-		close(fd);
-		return (0);
-	}
-
-	bytes = read(fd, buff, letters);
-
-	if (bytes == -1)
-	{
-		close(fd);
-		free(buff);
-		return (0);
-	}
-
-	r = write(STDOUT_FILENO, buff, bytes);
-
-	if (r == -1)
-	{
-		close(fd);
-		free(buff);
-		return (0);
-	}
-	close(fd);
-	return (bytes);
-}
+#endif
